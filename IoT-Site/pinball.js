c650a4200dc3a49e1ac8964c3d2811f673cedbf7
ZacharyDaulton <31374659+ZacharyDaulton@users.noreply.github.com>
2017-08-30 09:00:13 -0700

    Update pinball.js

--- IoT-Site/pinball.js
+++ IoT-Site/pinball.js
@@ -1,21 +1,21 @@
-// Code for rendering MatterJS objects in P5JS by Daniel Shiffman
-// http://codingtra.in
-// Original Code: https://youtu.be/uITcoKpbQq4
-// Changes made for IoT Showcase by Zachary Daulton @ZacharyDaulton
+// Original Code rendering MatterJS objects in P5JS by Daniel Shiffman: https://youtu.be/uITcoKpbQq4
+
+// All changes made for IoT Showcase by Zachary Daulton @ZacharyDaulton
+// All comments made by Zachary Daulton
 
 function Circle(x, y, r, g, c, m) {
   var options = {
     friction: 0,
     frictionStatic: 0,
     restitution: 0.8,
-    collisionFilter:{group: g, category: c, mask: m},
+    collisionFilter:{group: g, category: c, mask: m},    // Collision assignments hierarchy category and mask not needed at this time
 
   }
-  this.body = Bodies.circle(x, y, r, options);
+  this.body = Bodies.circle(x, y, r, options);          // r for radius, as opposed to r for render in boundary object
   this.r = r;
   World.add(world, this.body);
 
-  this.show = function() {
+  this.show = function() {                            // Renders the object through P5js instead of MatterJS
     var pos = this.body.position;
     var angle = this.body.angle;
     push();
