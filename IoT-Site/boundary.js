ad5f5202e8e3148fa2dd88bb603ff0473690e214
ZacharyDaulton <31374659+ZacharyDaulton@users.noreply.github.com>
2017-08-30 08:57:00 -0700

    Update boundary.js

--- IoT-Site/boundary.js
+++ IoT-Site/boundary.js
@@ -1,18 +1,19 @@
-// Code for rendering MatterJS objects in P5JS by Daniel Shiffman
-// http://codingtra.in
-// Original Code: https://youtu.be/uITcoKpbQq4
-// Changes made for IoT Showcase by Zachary Daulton @ZacharyDaulton
+// Original Code rendering MatterJS objects in P5JS by Daniel Shiffman: https://youtu.be/uITcoKpbQq4
+
+// All changes made for IoT Showcase by Zachary Daulton @ZacharyDaulton
+// All comments made by Zachary Daulton
 
 function Boundary(x, y, w, h, a, s, g, c, m, r) {
 
-this.ren = r;
+this.ren = r;                                              // variable for choosing to render the object in MatterJS. 
+                                                           // Compound object parts should not render, only the end result compound
 
   var options = {
     friction: 0,
-    restitution: .1,
+    restitution: .1,                                        // Object elasticity
     angle: a,
     isStatic: s,
-    collisionFilter:{group: g, category: c, mask: m},
+    collisionFilter:{group: g, category: c, mask: m},         // Collision assignments hierarchy
   }
 
 
@@ -22,12 +23,12 @@ this.ren = r;
   this.w = w;
   this.h = h;
 
-  if (this.ren == true){
+  if (this.ren == true){                                     // Add to the MatterJS world if render is true
   World.add(world, this.body);
   console.log(this.body);
   }
 
-  this.show = function() {
+  this.show = function() {                                  // Renders the object through P5js instead of MatterJS
     var pos = this.body.position;
     var angle = this.body.angle;
     push();
